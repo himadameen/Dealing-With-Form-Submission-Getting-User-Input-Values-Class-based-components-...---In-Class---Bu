f537c3e04155180e44378ae2add0bc878b3ed3de
Himmad Ameen <93907951+himadameen@users.noreply.github.com>
2022-06-01 16:04:06 +0530

    my changes

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,23 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+	    this.state = {
+	    	show : false
+	    }
 	};
+	
+	showText = () => {
+		this.setState({
+			show : true
+		})
+	}
 
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+			<button id="click" onClick={this.showText}>click me</button>
+                	{this.state.show && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes
+           			 me so happy</p>}
     		</div>
     	);
     }
